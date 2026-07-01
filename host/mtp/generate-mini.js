a53e5fbb1831a1c719d78b8b305161fe37bbbec3
scriptesting <ellexiusbrynn@gmail.com>
2026-07-01 11:41:39 -0600

    Add files via upload

---
+++ host/mtp/generate-mini.js
@@ -0,0 +1,66 @@
+// 丂匚尺丨卩ㄒ乇丂ㄒ
+// by Maddie
+
+var color;
+function changecolor() {
+    color = document.getElementById("playercolor").value;
+    document.getElementById("tracktitle").style.color = "#" + color;
+    document.getElementById("pause1").style.background = "#" + color;
+    document.getElementById("pause2").style.background = "#" + color;
+    document.getElementById("play").style.borderLeft = "6px solid " + "#" + color;
+    document.getElementById("skip1").style.borderLeft = "4px solid " + "#" + color;
+    document.getElementById("skip2").style.borderLeft = "4px solid " + "#" + color;
+}
+
+var autos = "off";
+function auto() {
+    if ( autos == "off" ) {
+        autos = "on";
+        document.getElementById("autobox").style.background = "#0CDB58";
+        document.getElementById("autobox").value = "autoplay is on";
+    }
+    else if ( autos == "on" ) {
+        autos = "off";
+        document.getElementById("autobox").style.background = "#FF0054";
+        document.getElementById("autobox").value = "autoplay is off";
+    }
+    else {}
+}
+
+function generate() {
+    document.getElementById("index-color-select").style.display = "none";
+    document.getElementById("code1").style.display = "block";
+    document.getElementById("code2").style.display = "block";
+    document.getElementById("desc").style.textAlign = "left";
+    document.getElementById("desc").innerHTML = "The preview for your custom music player isn't viewable here, but it will appear correctly once installed.<br><br><b>First code:</b> Paste just before <code>/body</code><br><br><b>Second code:</b> This is where you can add any amount of audio. Positioning and audio help is provided in the code comments. First paste after <code>body</code><br><br><span>💬 For additional support, reach me <a href = 'mailto:maddie.j@outlook.com'  target= '_blank'>here</a>.</span>"
+            
+    var design = "#" + color + " " + autos + " ";
+    var display = "0,0,";
+
+    var limit, url, url2, title, title2;
+
+    for (limit=1; limit < 7; limit++) {
+        url = "trackurl" + limit;
+        title = "trackname" + limit;
+        url2 = document.getElementById(url);
+        title2 = document.getElementById(title);
+        if ( url2.value != "" ) {
+            url2.value += " ";
+            design += url2.value;
+        }
+        if ( title2.value != "" ) {
+            title2.value += ",";
+            display += title2.value;
+        }
+    }
+
+    (function() {
+        var designlength = design.length - 1;
+        var designcompressed = design.slice(0,designlength);
+        var displaylength = display.length - 1;
+        var displaycompressed = display.slice(0,displaylength);
+
+        document.getElementById("code1").innerHTML = "&lt;script src&equals;&quot;https://scriptest.github.io/mtp/multituneplayer-mini.js&quot;&gt;&lt;&sol;script&gt;";
+        document.getElementById("code2").innerHTML = "<!--------------------------------------------------------------------------------  Positioning → The player is automatically positioned to be at the bottom left of your site. If you specifically want it somewhere, paste it there and replace BOTTOM with MARGIN-TOP and replace LEFT with MARGIN-LEFT. Adjust the margin numbers to your liking. Depending on your site's coding, you may have to remove POSITION:FIXED. Unlimited Audio → Simply separate the track URLs with a single space, and if you chose to display titles, seperate with a single comma. --------------------------------------------------------------------------------> &lt;div id&equals;&quot;mtplayer&quot; style=&quot;bottom: 16px; left: 5px; position: fixed;&quot;&gt;&lt;span id&equals;&quot;multi-info&quot; style&equals;&quot;display: none;&quot;&gt;" + designcompressed + "&lt;&sol;span&gt;&lt;span id&equals;&quot;multi-names&quot; style&equals;&quot;display: none;&quot;&gt;" + displaycompressed + "&lt;&sol;span&gt;&lt;&sol;div&gt;";
+    })();
+}
---
+++ host/mtp/multitune-minimize.js
@@ -0,0 +1,174 @@
+// 丂匚尺丨卩ㄒ乇丂ㄒ
+// by Maddie
+
+mtp();
+
+    function mtp() {
+    var soundtrackinfo = document.getElementById("multi-info2").innerHTML;
+    var automatic = soundtrackinfo.split(" ");
+    var jump = automatic.length - 1;
+
+    var soundtracktitles = document.getElementById("multi-names2").innerHTML;
+    var tracks = soundtracktitles.split(",");
+
+    (function() {
+         var limit = 0;
+         for ( limit; limit < automatic.length; limit++ ) {
+             if ( tracks[limit] === undefined ) {
+                 tracks[limit] = " ";
+                 }
+             else {}
+            }
+            return;
+    })();
+
+    document.getElementById("mtplayer2").innerHTML = '<audio id="audio" preload="auto"><source src="' + automatic[2] +'" type="audio/mpeg"></audio><div id="controls1"><div id="play"></div><div id="pause"><div id="pause1"></div><div id="pause2"></div><div id="isolate2"></div></div></div><div id="controls2"><div id="skip"><div id="skip1"></div><div id="skip2"></div><div id="isolate3"></div></div></div><div id="tracktitle"></div><div id="isolate"></div>';
+    document.getElementById("mtplayer2").style.display = "block";
+    document.getElementById("mtplayer2").style.width = "25px";
+    document.getElementById("mtplayer2").style.padding = "0px 15px";
+
+    var audio = document.getElementById("audio");
+    audio.style.display = "none";
+
+    document.getElementById("isolate").style.clear = "both";
+    document.getElementById("isolate2").style.clear = "both";
+    document.getElementById("isolate3").style.clear = "both";
+
+    var controls1 = document.getElementById("controls1");
+    controls1.style.display = "block";
+    controls1.style.float = "left";
+
+    var controls2 = document.getElementById("controls2");
+    controls2.style.display = "block";
+    controls2.style.float = "right";
+
+    var tracktitle = document.getElementById("tracktitle");
+    tracktitle.style.display = "block";
+    tracktitle.style.textAlign = "left";
+    tracktitle.style.fontFamily = "arial, sans-serif";
+    tracktitle.style.fontSize = "12px";
+    tracktitle.style.fontWeight = "600";
+    tracktitle.style.lineHeight = "13px";
+    tracktitle.style.marginTop = "-1px";
+    tracktitle.style.marginLeft = "32px";
+    tracktitle.style.width = "500px";
+    tracktitle.style.position = "absolute";
+    tracktitle.style.overflow = "hidden";
+    tracktitle.style.cursor = "default";
+    tracktitle.style.color = automatic[0];
+    tracktitle.innerHTML = tracks[2];
+
+    var play = document.getElementById("play");
+    play.onmouseover = function() { this.style.opacity = "1"; };
+    play.onmouseout = function() { this.style.opacity = "1"; };
+    play.style.display = "block";
+    play.style.width = "0px";
+    play.style.height = "0px";
+    play.style.borderTop = "5px solid transparent";
+    play.style.borderLeft = "7px solid " + automatic[0];
+    play.style.borderBottom = "5px solid transparent";
+    play.style.cursor = "pointer";
+
+    var pause = document.getElementById("pause");
+    pause.onmouseover = function() { this.style.opacity = "1"; };
+    pause.onmouseout = function() { this.style.opacity = "1"; };
+    pause.style.display = "none";
+    pause.style.cursor = "pointer";
+
+    var pause1 = document.getElementById("pause1");
+    var pause2 = document.getElementById("pause2");
+
+    pause1.style.display = "block";
+    pause1.style.float = "left";
+    pause1.style.marginRight = "2px";
+    pause1.style.width = "3px";
+    pause1.style.height = "8.5px";
+    pause1.style.marginTop = "0.5px";
+    pause1.style.background = automatic[0];
+
+    pause2.style.display = "block";
+    pause2.style.float = "left";
+    pause2.style.width = "3px";
+    pause2.style.height = "8.5px";
+    pause2.style.marginTop = "0.5px";
+    pause2.style.background = automatic[0];
+
+    var skip = document.getElementById("skip");
+    skip.onmouseover = function() { this.style.opacity = "1"; };
+    skip.onmouseout = function() { this.style.opacity = "1"; };
+    skip.style.display = "block";
+    skip.style.cursor = "pointer";
+
+    var skip1 = document.getElementById("skip1");
+    var skip2 = document.getElementById("skip2");
+
+    skip1.style.display = "block";
+    skip1.style.float = "left";
+    skip1.style.width = "0px";
+    skip1.style.height = "0px";
+    skip1.style.borderTop = "5px solid transparent";
+    skip1.style.borderLeft = "5px solid " + automatic[0];
+    skip1.style.borderBottom = "5px solid transparent";
+
+    skip2.style.display = "block";
+    skip2.style.float = "left";
+    skip2.style.width = "0px";
+    skip2.style.height = "0px";
+    skip2.style.borderTop = "5px solid transparent";
+    skip2.style.borderLeft = "5px solid " + automatic[0];
+    skip2.style.borderBottom = "5px solid transparent";
+
+    if ( automatic[1] == "on" ) {
+        audio.play();
+        play.style.display = "none";
+        pause.style.display = "block";
+    }
+     
+    else {}
+
+    play.onclick = function() {
+        this.style.display = "none";
+        pause.style.display = "block";
+        audio.play();
+        return;
+    };
+
+    pause.onclick = function() {
+        this.style.display = "none";
+        play.style.display = "block";
+        audio.pause();
+        return;
+    };
+
+    var list = 2;
+
+   function skipend() {
+        if ( list == jump ) {
+            audio.src = automatic[2];
+            tracktitle.innerHTML = tracks[2];
+            list = 2;
+            audio.play();
+            play.style.display = "none";
+            pause.style.display = "block";
+            return;
+            }
+        else {
+            ++list;
+            audio.src = automatic[list];
+            tracktitle.innerHTML = tracks[list];
+            audio.play();
+            play.style.display = "none";
+            pause.style.display = "block";
+            return;
+            }
+    }
+
+    skip.onclick = function() {
+        skipend();
+        };
+
+    audio.onended = function() {
+        skipend();
+    };
+
+};
